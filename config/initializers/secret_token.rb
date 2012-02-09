dff798b3d98f16c85c4ab0e1f82afc12d0258305
Rafal <rafal@codephonic.com>
2012-02-09 04:49:09 -0800

    first commit - RAFAL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babyfolio::Application.config.secret_token = '8437ed40f948868ad1f24a8c0d4f7f846d04ea3ba1997b6b9f186e4cd760a784874f9872534e2784c46b8b7334544b6c136d8f4c5157dc77119fe87577366591'
