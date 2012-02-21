1f9d9f937b18cd70909a77854749a46e5de72215
Rafal <rafal@codephonic.com>
2012-02-21 06:40:59 -0800

    creating form for inviting frends - RAFAL

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,4 +1,9 @@
 class FamiliesController < ApplicationController
+
+  before_filter :require_user
+  before_filter :require_confirmation
+  before_filter :require_family, :only => [:add_friends, :create_friend_relations]
+
   def new
     @family = Family.new
     10.times {
@@ -24,12 +29,17 @@ class FamiliesController < ApplicationController
     
     respond_to do |format|
       if @family.save
+
         second_relation = @family.relations.fetch(1);
         unless second_relation.nil?
           UserMailer.invite_user(second_relation.user, second_relation.member_type).deliver
         end
-        format.html { redirect_to :controller => 'home', :action => 'index' , :notice => 'Family has been successfully created.' }
-        format.xml { render :xml => @family, :status => :created, :location => @family }
+        
+        flash[:notice] = 'Family has been successfully created.'
+        session[:current_family] = @family
+        format.html { redirect_to add_friends_families_path }
+        format.xml  { render :xml => @family, :status => :created, :location => @family }
+        
       else
         @family.relations.first.user = User.new(:email => current_user.email)
         @family.relations.build(:user => User.new, :member_type => 'parent') if @family.relations.length == 1
@@ -49,34 +59,96 @@ class FamiliesController < ApplicationController
 
   def create_friend_relations
 
-    @error = false
-    users = Array.new
-
     @family = current_family
     @friends = params[:friends]
+    user_emails = Array.new
+    users = Array.new
+    @error = false
+
     @friends.each do |friend|
-      user = User.new(:email => friend[1][:email])
-      user.relations << Relation.new(:member_type => friend[1][:member_type], :family_id => @family.id)
-      user.reset_password
-      user.reset_perishable_token
-      user.reset_single_access_token
-      unless user.valid?
-        flash[:notice] = "Invalid emails"
-        @error = true
-        break
-      else
-        users << user
+      user_email = friend[1][:email].strip
+      unless  user_email == "";
+        unless user_emails.include?(user_email)
+          user_emails << user_email
+        else
+          @error = true
+          break
+        end
       end
     end
-    
+
     unless @error
-      users.each do |user|
-        user.save
+      exist_users = User.where(['email IN (?)', user_emails]).all
+      exist_users.each do |exist_user|
+        unless exist_user.families.empty?
+          exist_user.families.each do |family|
+            if family == @family
+             @error = true
+            end
+          end
+        end
       end
-      redirect_to add_friends_families_url
+    end
+
+    unless @error
+
+      @friends.each do |friend|
+        unless friend[1][:email].strip == ""
+
+          @user_exist = false
+          
+          exist_users.each do |exist_user|
+            if exist_user.email == friend[1][:email]
+              @user = exist_user
+              @user_exist = true
+              break
+            end
+          end
+
+          unless @user_exist
+            @user = User.new(:email => friend[1][:email])
+            @user.reset_password
+            @user.reset_perishable_token
+            @user.reset_single_access_token
+          end
+
+          @user.relations.build :member_type => friend[1][:member_type], :family_id => @family.id
+          
+          unless @user.valid?
+            @error = true
+            break
+          else
+            users << @user
+          end
+        end
+      end
+      unless @error
+        users.each do |user|
+          user.save
+          UserMailer.invite_user(user, user.relations.where(["family_id = ?",@family.id]).first.member_type).delivery
+        end
+        flash[:notice] = "Congrats you send emails to your friends :D!!"
+        redirect_to add_friends_families_url
+      else
+        flash[:notice] = "Invalid emails!"
+        render :action => :add_friends
+      end
     else
+      flash[:notice] = "Sorry one of the email you wrote is exist in your family"
       render :action => :add_friends
     end
   end
+
+  private
+
+    def require_family
+      redirect_to new_family_url unless current_family
+    end
+    
+    def require_no_family
+      redirect_to new_family_url if current_family
+    end
+
+
 
 end
--- test/functional/family_controller_test.rb
+++ test/functional/family_controller_test.rb
@@ -5,6 +5,7 @@ class FamiliesControllerTest < ActionController::TestCase
   def setup
     
     login_user
+    @family = families(:one)
     
   end
 
@@ -24,8 +25,7 @@ class FamiliesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create friend relations" do
-
+  test "should add 3 users" do
     assert_difference("User.count", 3) do
       assert_difference("Relation.count", 3) do
           
@@ -37,6 +37,10 @@ class FamiliesControllerTest < ActionController::TestCase
         assert_response :redirect
       end
     end
+  end
+
+  test "the user is exist in db" do
+    post :create_friend_relations, :friends => {"0" => { :email => 'raf.walczak@gmail.com', :member_type => 'cousin'}}
 
     assert_no_difference("User.count") do
       assert_no_difference("Relation.count") do
@@ -47,7 +51,9 @@ class FamiliesControllerTest < ActionController::TestCase
         assert_response :success
       end
     end
+  end
 
+  test "should not add user to db and should send error" do
     assert_no_difference("User.count") do
       assert_no_difference("Relation.count") do
         post :create_friend_relations, :friends => {
@@ -57,17 +63,54 @@ class FamiliesControllerTest < ActionController::TestCase
         assert_response :success
       end
     end
+  end
 
+  test "check if blank emails are added" do
     assert_difference("User.count") do
       assert_difference("Relation.count") do
         post :create_friend_relations, :friends => {
-          "0" => { :email => 'rafcio@gmail.com', :member_type => 'cousin'}
+          "0" => { :email => 'rafalek@gmail.com', :member_type => 'cousin'},
+          "1" => { :email => '  ', :member_type => 'grandmother'}
         }
-
+   
         assert_response :redirect
       end
     end
-    
   end
 
+  test "send 2 the same emails" do
+    assert_no_difference("User.count") do
+      assert_no_difference("Relation.count") do
+        post :create_friend_relations, :friends => {
+          "0" => { :email => 'next1@gmail.com', :member_type => 'cousin'},
+          "1" => { :email => '  ', :member_type => 'grandmother'},
+          "2" => { :email => 'next1@gmail.com', :member_type => 'uncle
+            '}
+        }
+
+        assert_response :success
+      end
+    end
+  end
+
+  test "check if add correct relations" do
+    post :create_friend_relations, :friends => {
+          "0" => { :email => 'rafalek@gmail.com', :member_type => 'cousin'},
+          "1" => { :email => '  ', :member_type => 'grandmother'}
+        }
+
+    user = User.where(["email = ?",'rafalek@gmail.com']).first
+    
+    assert_equal user.relations.first.member_type, 'cousin', 'CHECK IF THE MEMBER TYPA FIELD WAS ADDED'
+    assert_equal user.families.first.name, @family.name, 'CHECK IF THE FAMILY RELATION WAS ADDED'
+  end
+
+
+  test "check existing email" do
+    post :create_friend_relations, :friends => {
+          "0" => { :email => 'john@smith.com', :member_type => 'cousin'},
+          "1" => { :email => 'raf.walczak@gmail.com', :member_type => 'grandmother'}
+        }
+  end
+  
 end
