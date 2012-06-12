b5b16a1573b862279676e2ce6cc4eee43d2867ec
Rafal <rafal@codephonic.com>
2012-06-12 01:46:04 -0700

    Changing table logs ENGINE to MyISAM - RAFAL

---
+++ db/migrate/20120612084312_change_table_logs_options.rb
@@ -0,0 +1,23 @@
+class ChangeTableLogsOptions < ActiveRecord::Migration
+  def up
+    drop_table :logs
+    create_table :logs, :options => "ENGINE=MyISAM" do |t|
+
+      t.string :description
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :logs
+    create_table :logs do |t|
+
+      t.string :description
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
