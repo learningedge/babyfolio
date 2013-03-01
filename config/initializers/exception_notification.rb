ea454a8aa253fc517a2643bdd2a74c4af0cd339e
gutto <augustyn@codephonic.com>
2013-03-01 11:09:34 +0100

    fixing timeline comment image placeholder - gutto

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,5 +2,5 @@ if Rails.env == 'production'
   Babyfolio::Application.config.middleware.use ExceptionNotifier,
   :email_prefix => "[Babyfolio Exception] ",
   :sender_address => %{"Babyfolio" <info@babyfol.io>},
-  :exception_recipients => %w{lukasz@codephonic.com}
+  :exception_recipients => %w{team@codephonic.com}
 end
