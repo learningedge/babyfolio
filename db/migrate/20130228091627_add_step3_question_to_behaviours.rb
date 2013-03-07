925366f9568d86a086ec540ecc4f55f8b88be5e8
gutto <augustyn@codephonic.com>
2013-03-07 18:15:27 +0100

    taking out excel task from migration - gutto

--- db/migrate/20130228091627_add_step3_question_to_behaviours.rb
+++ db/migrate/20130228091627_add_step3_question_to_behaviours.rb
@@ -2,6 +2,6 @@ class AddStep3QuestionToBehaviours < ActiveRecord::Migration
   def change
     add_column :behaviours, :step3_question, :text
 
-    Rake::Task['excel:load_all_data'].invoke
+    #Rake::Task['excel:load_all_data'].invoke
   end
 end
