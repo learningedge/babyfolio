e7d69ed446ea50499675fb460ede3e8bbc6afb7d
Codephonic <lukasz@codephonic.com>
2013-04-18 09:00:06 -0700

    change migration - RAFAL

--- db/migrate/20130418083559_change_field_types_for_page_custom_fields.rb
+++ db/migrate/20130418083559_change_field_types_for_page_custom_fields.rb
@@ -1,6 +1,6 @@
 class ChangeFieldTypesForPageCustomFields < ActiveRecord::Migration
   def up
-    CustomField.where('field_type IN ("text_field", "text_area")').update_all(:field_type => 'wysiwyg');
+    CustomField.where('field_type = "text_field" OR field_type = "text_area"').update_all(:field_type => 'wysiwyg');
   end
 
   def down
