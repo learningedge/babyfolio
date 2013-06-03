c295c2729a8b0edaf18cbfb50abfff87ed0aa921
Codephonic <lukasz@codephonic.com>
2013-06-03 07:39:31 -0700

    Updated migration

--- db/migrate/20130529143014_add_is_welcome_program_enabled.rb
+++ db/migrate/20130529143014_add_is_welcome_program_enabled.rb
@@ -1,6 +1,6 @@
 class AddIsWelcomeProgramEnabled < ActiveRecord::Migration
   def up
-    add_column :user_options, :is_welcome_program_enabled, :boolean, :default => 0
+    add_column :user_options, :is_welcome_program_enabled, :boolean, :default => true
   end
 
   def down
